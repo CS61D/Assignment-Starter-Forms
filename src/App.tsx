791587a0929addded978c2be0372e2bb8fe662a9
Angela He <angelah0503@gmail.com>
2024-07-26 18:39:22 -0700

    merge

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Form from './components/Form'
+import './styles.css'
+
+export default function App() {
+
+  return (
+    <>
+      <h1>Sign Up</h1>
+      <p>* Required Field</p>
+      <Form />
+    </>
+  );
+}
